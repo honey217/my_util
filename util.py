369f4eaa5ed567723d7c74a65e1086d986ee1cd8
honey217 <thenmozhi21mit@gmail.com>
2026-05-19 19:44:45 +0530

    added new line

--- util.py
+++ util.py
@@ -2,7 +2,10 @@ import math
 number =25
 square_root=math.sqrt(number)
 power_value=math.pow(number,2)
+a=12
+b=5
 
+print(a+b)
 print("\nResults")
 print(f"squareroot of the number is:{square_root}")
 print(f"{number} raised to power 2:{power_value}")
