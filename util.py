c734c4ad5b6327ef9fd5cc852f0037f58b344dd1
honey217 <thenmozhi21mit@gmail.com>
2026-05-19 19:34:11 +0530

    initial project setup

---
+++ util.py
@@ -0,0 +1,8 @@
+import math
+number =25
+square_root=math.sqrt(number)
+power_value=math.pow(number,2)
+
+print("\nResults")
+print(f"squareroot of the number is:{square_root}")
+print(f"{number} raised to power 2:{power_value}")
